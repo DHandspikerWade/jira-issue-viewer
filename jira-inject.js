1509c3c6e64ef33f9bda25e7f9f99cee9ef72d89
Devin Handspiker-Wade <devin@spikedhand.com>
2015-09-10 22:56:08 -0300

    Removed console.log()

--- jira-inject.js
+++ jira-inject.js
@@ -8,10 +8,8 @@
 			}
 		}
 	}
-	
-	//----------TODO this sends WAY too many requests
+
 	elements = document.querySelectorAll('body.page-type-dashboard .issue-link[data-issue-key]');
-	console.log('Found ' + elements.length + ' issues');
 	if (elements.length) {;
 		for (i = 0; i < elements.length; i++) {
 			chrome.runtime.sendMessage({ type: "JIRA_ISSUE", key: elements[i].dataset.issueKey, visited: false});
